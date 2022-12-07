47e1c32fd01cc48b131c37a975ccbe5d9cb3d83c
Harshit Sharma <beyondtech.harshit@gmail.com>
2022-12-07 11:32:47 +0530

    Save Login Details

--- src/redux/userDataReducer.js
+++ src/redux/userDataReducer.js
@@ -2,15 +2,11 @@ export const USER_DETAILS = 'USER_DETAILS';
 export const REGISTERED = 'REGISTERED';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
-export const PROFILE_DATA = 'PROFILE_DATA';
-export const DASHBOARD = 'DASHBOARD';
 
 export const initialState = {
   login: false,
   register: false,
-  dashboard: false,
   user_details: {},
-  profile_data: {},
 };
 
 export const setLogin = value => ({
@@ -28,18 +24,8 @@ export const setUserDetails = value => ({
   payload: value,
 });
 
-export const setProfileData = value => ({
-  type: PROFILE_DATA,
-  payload: value,
-});
-
 export const setLogout = () => ({
   type: LOGOUT,
-});
-
-export const setDashboard = value => ({
-  type: DASHBOARD,
-  payload: value,
 });
 
 export const UserDataReducer = (state = initialState, action) => {
@@ -62,25 +48,12 @@ export const UserDataReducer = (state = initialState, action) => {
         user_details: action.payload,
       };
     }
-    case PROFILE_DATA: {
-      return {
-        ...state,
-        profile_data: action.payload,
-      };
-    }
-    case DASHBOARD: {
-      return {
-        ...state,
-        dashboard: action.payload,
-      };
-    }
     case LOGOUT: {
       return {
         ...state,
         login: false,
         register: false,
         user_details: {},
-        profile_data: {},
       };
     }
     default:
--- src/screens/Dashboard/Home.js
+++ src/screens/Dashboard/Home.js
@@ -1,7 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {useSelector} from 'react-redux';
 
 const Home = ({navigation}) => {
+  const UserData = useSelector(e => e?.UserDataReducer?.user_details);
+  console.log('User Data: ', UserData);
+
   return (
     <View
       style={{
