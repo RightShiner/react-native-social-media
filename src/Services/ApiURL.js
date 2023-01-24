6572c607af42b1cb27e00c4b635f2c8c635d0739
RightShiner <brilight.shiner@gmail.com>
2023-01-24 21:49:34 +0100

    feat:initial app

--- src/Services/ApiURL.js
+++ src/Services/ApiURL.js
@@ -1,4 +1,6 @@
-export const baseURL = 'http://socialmediacase.vnvserver.com/api';
+// export const baseURL = 'http://socialmediacase.vnvserver.com/api';
+// export const baseURL = 'http://localhost:9000/SocialMediaAcess-BE/public/api';
+export const baseURL = 'http://localhost:8080/SocialMediaAcess-BE/public/api/';
 export const SIGNUP = `${baseURL}/register`;
 export const LOGIN = `${baseURL}/login`;
 export const ASSESSMENT_FORM = `${baseURL}/questions`;
